5836f13356dbdf2855adacd6e108d9ae4d2a2207
SteakingCoder <1941499609@qq.com>
2018-05-20 23:57:29 +0800

    完成点击MainActivity页的'点击'按钮跳转至'百度地图'功能

--- app/src/main/java/wang/com/weatherforecast/MainActivity.java
+++ app/src/main/java/wang/com/weatherforecast/MainActivity.java
@@ -1,10 +1,15 @@
 package wang.com.weatherforecast;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +17,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<String> weatherItemAdapter;
     ListView weatherItemView;
+    Button menuBtn;
     private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +38,19 @@ public class MainActivity extends AppCompatActivity {
         weatherItemAdapter = new WeatherItemAdapter(MainActivity.this, R.layout.weather_item, weatherItems);
         weatherItemView = (ListView) findViewById(R.id.weather_item_view);
         weatherItemView.setAdapter(weatherItemAdapter);
+
+
+        menuBtn = (Button) findViewById(R.id.menuBtn);
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "点击", Toast.LENGTH_LONG).show();
+                intent = new Intent();
+                //TODO
+                intent.setData(Uri.parse(" baidumap://map/show?center=40.057406655722,116.29644071728&zoom=11&traffic=on&bounds=37.8608310000,112.5963090000,42.1942670000,118.9491260000"));
+                startActivity(intent);
+
+            }
+        });
     }
 }
