aa4f95d882b17a0755a0387e8669aeaac0664ba4
SteakingCoder <1941499609@qq.com>
2018-05-21 16:07:26 +0800

    更名内部类LatLng为CityDetail
    1.目的在于将该类应用于openweathermap.org的API
    2.添加成员变量于该类中，并求修改相应的构造函数

--- app/src/main/java/wang/com/weatherforecast/LocationMap.java
+++ app/src/main/java/wang/com/weatherforecast/LocationMap.java
@@ -8,25 +8,29 @@ import java.util.Map;
  */
 
 public class LocationMap {
-    private Map<String, LatLng> location = new HashMap<>();
+    private Map<String, CityDetail> location = new HashMap<>();
 
     public LocationMap(){
-        location.put("北京", new LatLng(116.422091,39.914714));
-        location.put("南京", new LatLng(118.794373,32.064897));
-        location.put("长沙", new LatLng(112.985139,28.246855));
-        location.put("广州", new LatLng(113.267981,23.127094));
+        location.put("北京", new CityDetail(2038349, "Beijing Shi",116.422091,39.914714));
+        location.put("南京", new CityDetail(1799963, "Nanjing", 118.794373,32.064897));
+        location.put("长沙", new CityDetail(1815577, "ChangSha", 112.985139,28.246855));
+        location.put("广州", new CityDetail(1809858, "Guangzhou", 113.267981,23.127094));
     }
 
-    public LatLng getLalo(String cityName){
+    public CityDetail getCityDetail(String cityName){
 
         return location.get(cityName);
     }
 
-    private class LatLng{
+    private class CityDetail {
+        private int id;
+        private String code;
         private double lat;
         private double lng;
 
-        public LatLng(double lat, double lng){
+        public CityDetail(int id, String code, double lat, double lng){
+            this.code = code;
+            this.id = id;
             this.lat = lat;
             this.lng = lng;
         }
