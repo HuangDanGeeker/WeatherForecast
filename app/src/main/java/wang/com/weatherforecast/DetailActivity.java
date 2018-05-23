c3f1c19837de0d3b5a2f2437234747679bbe73cb
SteakingCoder <1941499609@qq.com>
2018-05-24 02:37:20 +0800

    为DetailActivity标题栏中的图片设置点击响应失败

--- app/src/main/java/wang/com/weatherforecast/DetailActivity.java
+++ app/src/main/java/wang/com/weatherforecast/DetailActivity.java
@@ -1,11 +1,21 @@
 package wang.com.weatherforecast;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class DetailActivity extends Activity {
+
+    WeatherItem weatherItem;
 
     private TextView dateTextView ;
     private TextView weekTextView ;
@@ -16,6 +26,9 @@ public class DetailActivity extends Activity {
     private TextView pressureTextView ;
     private TextView windTextView ;
 
+    private ImageView shareImgView;
+    private ImageView msgImgView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +37,7 @@ public class DetailActivity extends Activity {
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,      // 注意顺序
                 R.layout.activity_detail_title);
 
-        WeatherItem weatherItem = (WeatherItem) getIntent().getSerializableExtra("WeatherItem");
+        weatherItem = (WeatherItem) getIntent().getSerializableExtra("WeatherItem");
 
         weekTextView = (TextView) findViewById(R.id.weekTextView);
         weatherTypeTextView = (TextView) findViewById(R.id.weatherTypeTextView);
@@ -41,6 +54,42 @@ public class DetailActivity extends Activity {
         humidityTextView.setText(weatherItem.getHumidity());
         pressureTextView.setText(weatherItem.getPressure());
         windTextView.setText(weatherItem.getWind());
+
+        LayoutInflater factory = LayoutInflater.from(DetailActivity.this);
+        View layout = factory.inflate(R.layout.activity_detail_title, null);
+        shareImgView = (ImageView) layout.findViewById(R.id.shareImg);
+        msgImgView = (ImageView) layout.findViewById(R.id.msgImg);
+
+        shareImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailActivity.this, "mmmmm", Toast.LENGTH_SHORT).show();
+                SmsManager smsManager = SmsManager.getDefault();
+                String content = weatherItem.toString();
+                ArrayList<String> list = smsManager.divideMessage(content);
+                for (int i = 0; i < list.size(); i++) {
+                    smsManager.sendTextMessage("10086", null, list.get(i), null, null);
+                }
+            }
+        });
+
+        msgImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DetailActivity.this, "sssss", Toast.LENGTH_SHORT).show();
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, weatherItem.toString());
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent, "Share to..."));
+            }
+        });
+
+
+
     }
+
+
+
 
 }
